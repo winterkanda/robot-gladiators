3a58502bbe2213f91e6f9468f8d759a45fd14ca7
Kiran Kanda <kkanda@dons.usfca.edu>
2021-04-28 18:13:33 -0700

    added final points and game over

--- assets/js/game.js
+++ assets/js/game.js
@@ -85,30 +85,30 @@ var fight = function(enemyName) {
     }
 };  
 
-if (playerHealth > 0) {
-    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+// fight each enemy-robot by looping over them and fighting them one at a time
+for(var i = 0; i < enemyNames.length; i++) {
+    // if player is still alive, keep fighting
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
   
-    // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
 
-    // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
 
-    // use debugger to pause script from running and check what's going on at that moment in the code
-    // debugger;
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
 
-    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedEnemyName);
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
 
-    for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    // if player isn't alive, stop the game
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
     }
 }
 
-else {
-    window.alert("You have lost your robot in battle! Game Over!");
-    break;
-  }
